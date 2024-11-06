607fef138a9c0bd5a2eb7732f5f5cfb97b8b392b
devcecode <devcecode@gmail.com>
2024-11-06 17:16:54 -0500

    v1

---
+++ lib.js
@@ -0,0 +1,22 @@
+import {Resend} from "resend";
+import axios from "axios";
+
+export async function SendEmail() {
+  const { data: htmlContent } = await axios.get('https://customer-scopes-project.netlify.app/email-template.html');
+
+  const resend = new Resend('re_FbfJ8kd1_Q8etDXxyW1ESCVQfPusoncze');
+
+  try {
+    const response = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: 'devcecode@gmail.com',
+      subject: 'Hello World',
+      html: htmlContent
+    });
+  
+    console.log(response)
+    
+  } catch (error) {
+    console.log(error.message)
+  }
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { SendEmail } from "./lib.js";
+import cors from 'cors'
+
+import dotenv from "dotenv"; // Asegúrate de tener la función de enviar el correo
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+var corsOptions = {
+  origin: ['https://customer-scopes-project.netlify.app', 'http://localhost:5173'],
+  credentials: true
+}
+
+app.use(cors(corsOptions))
+
+app.get("/api/send-email", async (req, res) => {
+  try {
+    await SendEmail();
+    res.status(200).send("Correo enviado");
+  } catch (error) {
+    res.status(500).send("Error al enviar el correo");
+  }
+});
+
+app.listen(3000, () => console.log("Servidor funcionando en puerto 3000"));
